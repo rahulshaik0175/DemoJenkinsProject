7e6f8a86f6829d005eae367814041e567a2d500c
rahulshaik0175 <rahulshaik0175@gmail.com>
2024-12-17 21:39:42 +0530

    Update HelloWorlController.java

--- HelloWorld/src/main/java/com/example/demo/controller/HelloWorlController.java
+++ HelloWorld/src/main/java/com/example/demo/controller/HelloWorlController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorlController {
-	
-	 @GetMapping("/hello")
-	    public String sayHello() {
-	        return "Hello, World!";
-	    }
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, World!";
 	}
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
